595c9f11133fa1f90a5df087c9901d65b86f10e5
Sandra13988 <sandra13988@gmail.com>
2024-07-31 12:43:52 +0200

    CSS de los componentes compartidos añadidos

--- spotify/src/app/app.routes.ts
+++ spotify/src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import { ExampleComponent } from './example/example.component';
 import { PruebaComponent } from './prueba/prueba.component';
+import { HomePageComponent } from './modules/home/pages/home-page/home-page.component';
 
 export const routes: Routes = [
     {
         path:'', //Localhost:4200/home -> si quisieras hacer /home/dashboard, el segundo caracter debes ponerselo al hijo, osea en home-routing.module.ts 
        // component: ExampleComponent -> Esto es sin lazy loading, osea se cargaria entero, no segun la necesidad del cliente
-       loadChildren:() => import('./modules/home/home.module').then(m => m.HomeModule) //importar modulo
+       //loadChildren:() => import('./modules/home/home.module').then(m => m.HomeModule) //importar modulo
+       component: HomePageComponent
 
     },
     {
--- spotify/src/app/modules/home/pages/home-page/home-page.component.ts
+++ spotify/src/app/modules/home/pages/home-page/home-page.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { SideBarComponent } from '../../../../shared/components/side-bar/side-bar.component';
+import { HeaderuserComponent } from '../../../../shared/components/headeruser/headeruser.component';
+import { MediaplayerComponent } from '../../../../shared/components/mediaplayer/mediaplayer.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [SideBarComponent],
+  imports: [
+    SideBarComponent,
+    HeaderuserComponent,
+    MediaplayerComponent
+  ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
